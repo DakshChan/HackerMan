0aeefb045778621250110e2f8a630af195de6fe6
Racer3451 <Racer3451@users.noreply.github.com>
2019-06-10 11:12:21 -0400

    Added warcrimes

---
+++ src/warcrimes.java
@@ -0,0 +1,15 @@
+/*
+import jordan.minecraft.punjisticks;
+
+class warcrimes{
+	public static void main(String[] args){
+		pain();
+	}
+	public static void pain(){
+		commitwarcrimes();
+	}
+	public static void commitwarcrimes(){
+		System.out.println("Oh no");
+	}
+}
+*/

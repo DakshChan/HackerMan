86c71f63ec158a28104ca35a8ebffd6e9b7eab18
Racer3451 <Racer3451@users.noreply.github.com>
2019-06-10 20:22:53 -0400

    Fixed Jordans floor entity

--- src/Floor.java
+++ src/Floor.java
@@ -1,7 +1,7 @@
 /**
  * Floor.java
- * Version 1.0
- * Author: @ Jordan A.
+ * Version 1.1
+ * Authors: @ Jordan A. @ Daksh M
  * @ 05/30
  * A class for floors
  */
@@ -15,8 +15,8 @@ class Floor extends Entity{
   
   Image sprite;
     
-  Floor(int x, int y, int facing, int size, String name){
-    super(x, y, facing, size);
+  Floor(int x, int y, int facing, String name){
+    super(x, y, facing);
     loadSprite(name);
   }
   
@@ -28,11 +28,12 @@ class Floor extends Entity{
     }
     
     // scales the image up to the screen size
-    sprite = sprite.getScaledInstance(Size, Size, Image.SCALE_SMOOTH);
+    sprite = sprite.getScaledInstance(Entity.Size, Entity.Size, Image.SCALE_SMOOTH);
     
   }
-  
-  void drawSelf(Graphics g){
+
+  @Override
+  public void drawSelf(Graphics g){
     g.drawImage(sprite, x, y, null);
   }
 }
